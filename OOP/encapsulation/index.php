f319b1075958eaba37858ffcc0add52b4aef8c79
Thomas <tg.59290@gmail.com>
2020-05-05 15:41:02 +0200

    add exercice on encapsulation

--- OOP/encapsulation/index.php
+++ OOP/encapsulation/index.php
@@ -1,35 +1,41 @@
 <?php
 // Exercice 1
 echo "<h2>Exercice 1</h2>";
-// Add the content of dog.php file in index.php
-require "dog.php";
-// Create two instances of the Dog class
-$dog1 = new Dog();
-$dog2 = new Dog();
-// Store the $dog2 object in the $dog3 variable but they both reference to the same object
-$dog3 = $dog2;
+require "student.php";
+// Instanciate the studen object
+$student = new Student();
+// Setting attribut's values directly
+// $student->name = "Timmy";
+// $student->age = 35;
 
-var_dump($dog1);
-var_dump($dog2);
-var_dump($dog3);
+// Using setters to give values to attributs
+$student->setName("Timmy");
+$student->setAge(35);
+var_dump($student);
 
 // Exercice 2
 echo "<h2>Exercice 2</h2>";
-require "customer.php";
-$customer = new Customer();
-// $customer store an empty object
-var_dump($customer);
-// Add one attribut name containing a string with walue Doe
-$customer->name = "Doe";
-var_dump($customer);
-// Add other attributs
-$customer->firstname = "John";
-$customer->age = 42;
-$customer->payments = ["visa", "mastercard", "paypal"];
-var_dump($customer);
-// Create a blank new instance of the Customer class without the previous attributs
-$customer2 = new Customer();
-var_dump($customer2);
+$student = new Student();
+// try to call the setters with values but we do not know yet if they are correct
+try {
+  $student->setName("a");
+  $student->setAge(1250);
+}
+// If they are not we catch the exception and display the message
+catch (\Exception $e) {
+  echo $e->getMessage();
+}
+var_dump($student);
 
-$customer->dog = $dog1;
-var_dump($customer);
+// Exercice 3
+echo "<h2>Exercice 3</h2>";
+require "book.php";
+$data = [
+  "title" => "The rain",
+  "pages" => 246,
+];
+// Hydrate the book with the data from the array
+$book = new Book($data);
+var_dump($book);
+// Display a basic sentence using the getters and not the attributs themselves because they are protected
+echo "<br>" . $book->getTitle() . " has " . $book->getPages() . " pages";
